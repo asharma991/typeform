d3b58d523789ef7cbc74767803e5014c8b28d385
vinaypokra <vvinayppokra@gmail.com>
2021-12-11 01:28:16 +0530

    Recoil added

--- tgs-typeform/src/index.js
+++ tgs-typeform/src/index.js
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
+import { RecoilRoot } from "recoil";
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </ThemeProvider>,
   document.getElementById("root")
 );
