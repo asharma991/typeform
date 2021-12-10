49f68e81e172d33a8f34083f3f7e73b6e06e5812
Abhishek Sharma <asharma991@outlook.com>
2021-12-11 00:48:41 +0530

    added theme

--- tgs-typeform/src/theme.js
+++ tgs-typeform/src/theme.js
@@ -1,14 +1,68 @@
 import { createTheme } from "@mui/material/styles";
-import { green, purple } from "@mui/material/colors";
 
 const theme = () =>
   createTheme({
     palette: {
       primary: {
-        main: purple[500],
+        main: "red",
       },
       secondary: {
-        main: green[500],
+        main: "#f44336",
+      },
+      action: {
+        disabled: "grey",
+      },
+    },
+    spacing: (value) => `${value * 8}px`,
+    typography: {
+      color: "blaclk",
+
+      root: {
+        color: "black",
+      },
+      body: {
+        fontSize: "16px",
+      },
+      body1: {
+        fontSize: "16px",
+      },
+      h1: {
+        fontSize: "3rem",
+        lineHeight: "54px",
+      },
+      h2: {
+        fontSize: "2.5rem",
+        lineHeight: "48px",
+      },
+      h3: {
+        fontSize: "2rem",
+        lineHeight: "36px",
+      },
+      h4: {
+        fontSize: "1.5rem",
+        lineHeight: "28px",
+      },
+      h5: {
+        fontSize: "1.125rem",
+        lineHeight: "24px",
+      },
+      h6: {
+        fontSize: "1rem",
+        lineHeight: "22px",
+      },
+      caption: {
+        fontSize: "0.8rem",
+        lineHeight: "24px",
+      },
+      subtitle1: {
+        fontSize: "1rem",
+        lineHeight: "22px",
+        fontFamily: "Roboto",
+      },
+      subtitle2: {
+        fontSize: "0.8rem",
+        lineHeight: "22px",
+        fontFamily: "Roboto",
       },
     },
   });
